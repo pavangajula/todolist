c6bc2aaa95d257cdaf279389adc1d76f88586479
pavangajula <pavan.gajula@copart.com>
2022-04-12 17:50:32 +0530

    getting inputtext into text

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
-function App() {
+const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Pavan's Todo List</h1>
       </header>
-      <Form />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
       <TodoList />
     </div>
   );
-}
+};
 
 export default App;
